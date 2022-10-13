02c8a08c87ae97e646721383fd41704d2267ac43
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-13 15:22:08 +0700

    Add Task00 Задача 1

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1. Вывести квадрат целого числа
+Console.Write("Введите целое число: ");//текст для пользователя
+int number = Convert.ToInt32(Console.ReadLine());//считываем и конвертируем данные с консоли в целочисленный тип
+
+int souare = number * number;//ищем квадрат
+Console.WriteLine($"Квадрат числа {number} = {souare} ");//вывод результата 
+
+
+//Задача 2. 
