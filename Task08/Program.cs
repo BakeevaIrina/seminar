29f731cafe2ed66b580566ce79db5898d75c0b68
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-14 13:28:39 +0700

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        int even = count % 2;
+        if (even == 0)
+        {
+            Console.Write($"{count} ");
+            count++;
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+}
+
+else
+{
+    Console.Write("Введите целое положительное число");
+}
