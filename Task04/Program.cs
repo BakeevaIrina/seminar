407c2fcf621f7f8b9a6513ba6fe39f669a2bf504
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-14 12:54:46 +0700

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if ( numberB > max )
+{
+    max = numberB;
+}
+if ( numberC > max)
+{
+    Console.Write($"max = {numberC}");
+}
+else
+{
+    Console.Write($"max = {numberA} ");
+}
