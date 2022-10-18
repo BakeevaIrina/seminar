cdb144f14b89b63aab8db22c09c659851aa1b7e7
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-18 14:37:37 +0600

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное = {number}");
+
+
+//решаем методом
+int Digit (int num)//method
+{
+    int firstDigit = num / 100 *10; // 784/100=7
+    int secondDigit = num % 10; // 784%10=4
+    return firstDigit + secondDigit;//тернарный оператор
+}
+int digit = Digit(number);
+Console.WriteLine($"Наибольшая цифра числа {number} равна {digit}");
