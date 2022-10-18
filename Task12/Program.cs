4f339b3b73eb1fe3893fdd88989865fc19ecaa44
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-18 15:36:07 +0600

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+if (numberA % numberB == 0) Console.WriteLine($"Кратное!");
+
+else  Console.WriteLine($"Не кратное, остаток - {numberA % numberB}");
