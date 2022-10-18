a419a9b835ea0ec9eaa56bdb7ef0cba092525ad3
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-18 14:22:07 +0600

    Task09 Method

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,9 +8,9 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 = {number}");
-
+/*
 int firstDigit = number / 10; // 78/10=7
-int secondDigit = number % 10; // 78%10=8
+int secondDigit = number % 10; // 78%10=8*/
 //1
 //if (firstDigit > secondDigit) System.Console.WriteLine($"Наибольшая цифра числa = {firstDigit}");
 //else Console.WriteLine($"Наибольшая цифра числa = {secondDigit} ");
@@ -18,7 +18,7 @@ int secondDigit = number % 10; // 78%10=8
 //АНАЛОГ1
 //int res = Math.Max(firstDigit, secondDigit);
 //Console.WriteLine(res);
-
+/*
 //Условный оператор
 int result = default;//значение по умолчанию, для int это 0, пишем для удобства, значение не определено 
 if (firstDigit > secondDigit) result = firstDigit;
@@ -31,7 +31,15 @@ Console.WriteLine($"Наибольшая цифра числa = {maxDigit} ");
 
 //Тернарный оператор 2
 Console.Write($"Наибольшая цифра числa {number} равна  ");
-Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);*/
 
 
-
+//решаем методом
+int MaxDigit (int num)//method
+{
+    int firstDigit = num / 10; // 78/10=7
+    int secondDigit = num % 10; // 78%10=8
+    return firstDigit > secondDigit ? firstDigit : secondDigit;//тернарный оператор
+}
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа равна {maxDigit}");
