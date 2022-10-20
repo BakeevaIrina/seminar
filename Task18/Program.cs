f63aa0403117bf3520926e995e91803e885a9fc9
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-20 14:19:38 +0600

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+
+string Querter(int num)
+{
+    if (num == 1) return "x>0, y>0";
+    if (num == 2) return "x<0, y>0";
+    if (num == 3) return "x<0, y<0";
+    if (num == 4) return "x>0, y<0";
+
+    return "Введен неверный носер четверти";
+}
+
+Console.WriteLine(Querter(quarter));
