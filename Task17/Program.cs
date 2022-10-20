c3c4c3de53c48db6cbf10ac9f5ad4f542a0ca333
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-20 13:31:40 +0600

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите точки");
+Console.WriteLine("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Querter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+}
+
+int querter = Querter(x, y);
+string result = querter > 0 
+                 ? $"Указанные координаты соответствуют четверти -> {querter}" 
+                 : "Введены не корректные координаты";
+Console.WriteLine(result);
