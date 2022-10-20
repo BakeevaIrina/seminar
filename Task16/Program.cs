aa38b362d7d819d7d065bdf232f59fe870b98848
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-20 12:39:37 +0600

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+
+bool Square( int numA, int numB)//method
+{
+    int result = numB * numB;
+    if (result == numA) return true;
+    return false;
+}
+
+bool result = Square(firstDigit, secondDigit);
+if (result == true) Console.WriteLine($"Да");
+else Console.WriteLine($"Нет");
