8d529db2e80a8649cef9f1c150569cd27fde1ce1
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-20 12:20:13 +0600

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberA = 7;
+int numberB = 23;
+
+bool Multiple(int digit, int numA, int numB)//method
+{
+    int result1 = digit % numA;
+    int result2 = digit % numB;
+    if (result1 == 0 && result2 == 0) return true;
+    return false;
+}
+
+bool result = Multiple (number, numberA, numberB);
+if (result == true) Console.WriteLine($"Кратное");
+else Console.WriteLine($"НЕ кратное");
