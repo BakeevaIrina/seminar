770f1e77e81a103bec73fe5f4bd86089bd9c6853
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-21 11:34:45 +0600

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 10: Напишите программу, которая 
+//1. принимает на вход трёхзначное число и 
+//2. на выходе показывает вторую цифру этого числа. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Div(int num)
+{
+    int firstDigit = num / 10;
+    return firstDigit % 10;
+}
+
+int result = Div(number);
+Console.WriteLine(result);
+
