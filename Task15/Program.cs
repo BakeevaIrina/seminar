7ab987d57ada82ce8e01c135f76efeef97e2da96
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-28 17:37:14 +0600

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 15: Напишите программу, которая 
+// 1. принимает на вход цифру, обозначающую день недели, и 
+// 2. проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+
+Console.WriteLine("Введите число от 1 до 7: ");
+string number = Console.ReadLine();
+
+
+string Day(string num)
+{
+    if (num == "1") return "нет";
+    if (num == "2") return "нет";
+    if (num == "3") return "нет";
+    if (num == "4") return "нет";
+    if (num == "5") return "нет";
+    if (num == "6") return "да";
+    if (num == "7") return "да";
+
+    return "Введены некоректные данные";
+}
+
+string result = Day(number);
+Console.WriteLine(result);
--- Task18/Program.cs
+++ Task18/Program.cs
@@ -2,7 +2,7 @@
 string quarter = Console.ReadLine();
 
 
-string Querter(int num)
+string Querter(string num)
 {
     if (num == "1") return "x>0, y>0";
     if (num == "2") return "x<0, y>0";
