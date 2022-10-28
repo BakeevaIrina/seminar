3cbeeccfd76516ef6a18afc6b8b55bb712bdfc01
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-28 18:13:25 +0600

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 19
+//Напишите программу, которая 
+// 1. принимает на вход пятизначное число и 
+//2. проверяет, является ли оно палиндромом.
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите пятизначное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Palindrome(int num)
+{
+    int integer = num / 1000; //23
+    int even = num % 100;//32
+    int res = integer - even;
+    return res == 9 || res == -9 ? true : false;
+}
+
+bool result = Palindrome(number);
+if (result == true) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
