90f3688d3a8c8653f0a224cb90a92023a4e70391
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-11-20 14:30:33 +0600

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 26
+//Напиште программу, к-ая принимает на вход число и
+//выдает количество цифр в числе
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Digit(int num)
+{
+    num = Math.Abs(num);//ставит число по модулю
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count ++;
+    }
+    return count > 0 ? count : 1;
+}
+
+int result = Digit(number);
+Console.WriteLine(result);
