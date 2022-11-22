1c1f5f45bc70203341cd6b5a83082f9acfa77569
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-11-22 12:23:11 +0600

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 29: Напишите программу, которая 
+//задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int num = 8;
+
+int[] NameArray(int number)
+{
+    int[] array = new int[number];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0,100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write("]");
+}
+
+int[] result = NameArray(num);
+PrintArray(result);
