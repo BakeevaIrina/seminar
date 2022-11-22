5a6c186200279ab30b77430df2227c17bade55e7
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-11-22 19:45:44 +0600

    Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 32: 
+//Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();//random - тип данных
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] InversionArray(int[]array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i] * -1;
+    }
+    return newArray;
+}
+
+void PrintArray (int[] array)//void тип данных, который не возвращает значения
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10);
+int[] inversionArray = InversionArray(arr);
+PrintArray(arr);
+PrintArray(inversionArray);
