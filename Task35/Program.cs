7ce087798360570c7356a8082bf29f75d48ba346
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-11-24 15:12:02 +0600

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 35: 
+//Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();//random - тип данных
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)//void тип данных, который не возвращает значения
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+int GetCountElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99) count ++;
+    }
+    return count;
+}
+
+
+int[] arr = CreateArrayRndInt(123, 0, 200);
+PrintArray(arr);
+int result = GetCountElements(arr);
+Console.WriteLine($"количество элементов массива bp отрезкf [10,99] = {result}");
