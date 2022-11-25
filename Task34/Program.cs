0a09061124d88f92d63baf034d9c438f71a863af
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-11-25 21:09:03 +0600

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 34: 
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();//random - тип данных
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)//void тип данных, который не возвращает значения
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count ++;
+    }
+    return count;
+}
+
+
+int[] arr = CreateArrayRndInt(5, 100, 999);
+PrintArray(arr);
+
+int result = CountNumbers(arr);
+Console.WriteLine($"Количество чётных чисел в массиве -> {result}");
