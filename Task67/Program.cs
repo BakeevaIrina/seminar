c6329b28882b063e70e8cf2ec9d515321260f102
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-12-13 22:46:45 +0600

    Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 67:
+// Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.WriteLine("Введите натурально число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumElementsNumber (int num)
+{
+    if(num == 0) return num;
+    return num % 10 + SumElementsNumber(num / 10);
+}
+
+int result = SumElementsNumber(number);
+Console.WriteLine(result);
